a6f15f14efce3e15f426863476fe45943a3d287a
xbhfree <xbhfree@163.com>
2023-07-05 20:55:25 +0800

    IoC-基于XML管理Bean-依赖注入-构造器注入

--- spring6-ioc-xml/src/main/java/com/spring6/iocxml/di/Book.java
+++ spring6-ioc-xml/src/main/java/com/spring6/iocxml/di/Book.java
@@ -1,8 +1,12 @@
 package com.spring6.iocxml.di;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
     private String bname;
--- spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestBook.java
+++ spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestBook.java
@@ -12,4 +12,19 @@ public class TestBook {
         Book book = context.getBean("book_setter_di",Book.class);
         System.out.println(book);
     }
+
+
+    @Test
+    public void testBookConstructorByName(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean-di.xml");
+        Book book = context.getBean("book_constructor_di_byname",Book.class);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testBookConstructorByIndex(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean-di.xml");
+        Book book = context.getBean("book_constructor_di_byindex",Book.class);
+        System.out.println(book);
+    }
 }
