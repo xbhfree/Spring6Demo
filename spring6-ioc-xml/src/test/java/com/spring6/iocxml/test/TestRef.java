1b3fdeff1211871ca034b3a6c1bdc9bd93befdf3
xbhfree <xbhfree@163.com>
2023-08-20 03:10:17 +0800

    Spring6框架-容器：IoC-基于XML管理Bean-依赖注入-对象类型属性（中）

--- spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestRef.java
+++ spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestRef.java
@@ -8,11 +8,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class TestRef {
+    //外部bean
     @Test
     public void testDeptAndEmp(){
         ApplicationContext context = new ClassPathXmlApplicationContext("bean-ref.xml");
         Department department = (Department)context.getBean("department");
         Employee employee = (Employee)context.getBean("employee");
+        employee.work();
+    }
+    //内部bean
+    @Test
+    public void testDeptAndEmp2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean-ref.xml");
+        Employee employee = (Employee)context.getBean("employee2");
         employee.work();
     }
 }
