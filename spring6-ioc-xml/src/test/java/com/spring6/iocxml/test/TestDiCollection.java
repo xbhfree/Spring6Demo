59fb25a71e5474c14d312107f2500a7e7be77123
xbhfree <xbhfree@163.com>
2023-09-10 03:12:49 +0800

    IoC-基于XML管理Bean-依赖注入-p命名空间

--- spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestDiCollection.java
+++ spring6-ioc-xml/src/test/java/com/spring6/iocxml/test/TestDiCollection.java
@@ -9,10 +9,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 测试注入集合bean
  */
 public class TestDiCollection {
+    /*测试注入bean集合*/
     @Test
     public void testDiCollection(){
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.diCollection.xml");
         Department department = (Department) context.getBean("Dept");
+        department.info();
+    }
+
+    /*测试命名空间*/
+    @Test
+    public void testDiP(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.diCollection.xml");
+        Department department = (Department) context.getBean("DeptP");
         department.info();
     }
 }
