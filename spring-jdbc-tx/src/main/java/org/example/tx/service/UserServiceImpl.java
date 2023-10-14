df75d5ea74761b164dacfbeb1b8cc5fa62c180ca
xbhfree <xbhfree@163.com>
2023-10-15 03:12:10 +0800

    Spring6框架-事务-基于注解的声明式事务-案例添加事务

--- spring-jdbc-tx/src/main/java/org/example/tx/service/UserServiceImpl.java
+++ spring-jdbc-tx/src/main/java/org/example/tx/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.tx.dao.BookDaoImpl;
 import org.example.tx.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -13,6 +14,8 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private BookDao bookDao;
+    //事务注解，可以加方法或者类上
+    @Transactional
     @Override
     public void buyBook(Integer bookId, Integer userId) {
         // 查询书价格
